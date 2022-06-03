128a7a7a3793e9e259012add7c309c1199b034b3
Theodore <30gash@gmail.com>
2022-06-02 21:31:24 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main () {
+int main(void)
+	{
 	char mystr[] = "\"Programming is like building a multilingual puzzle";
-	puts (mystr);
-	return 0;
-	}	
+
+	puts(mystr);
+	return (0);
+	}
