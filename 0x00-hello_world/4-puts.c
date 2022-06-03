e39ff556d0b9f567dd39d4e9825ee799c6c9911a
Theodore <30gash@gmail.com>
2022-06-02 23:12:43 -0700

    5. Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+/**
+* main(): -This is the main entry point for my program
+*/
 int main(void)
 	{
 	char mystr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(mystr);
 	return (0);
+	/* 'return()': - return 0 for success */
 	}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* Initial short description */
+
+/**
+ * main: description for main
+ */
+int main(void)
+	{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	/**
+	 * return: description for return
+	 */
+	return (0);
+	}
